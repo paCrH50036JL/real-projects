9a0d04e9ff9252b1966ccdd928bda1f47b2a2487
Your Name <you@example.com>
2021-04-04 01:22:22 +0800

    加入测试功能

--- 自动搜索.py
+++ 自动搜索.py
@@ -36,45 +36,55 @@ if __name__ == "__main__":
     chrome_options.add_argument('--start-maximized')
     executable_path = r'C:\Program Files\Google\Chrome\Application\chromedriver.exe'
     driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
-    driver.get("https://www3.wipo.int/branddb/en/#")
-    ### 进行操作
-    # 点击'FILTER BY'一侧
-    print('1')
-    driver.save_screenshot("debug1.png")
-    click_element(driver, "//*[@id='source_filter']/div[1]/div/div[6]/div/a[60]/div[1]/label")
-    click_element(driver, "//*[@id='ui-id-10']/span")
-    click_element(driver, "//label[@for='ACT_check']")
-    click_element(driver, "//label[@for='PEND_check']")
-    click_element(driver, "//*[@id='status_filter']/a/span")
-    # 点击'SEARCH BY'一侧
-    print('2')
-    driver.save_screenshot("debug2.png")
-    click_element(driver, "//*[@id='brand_search_line_0']/div[2]/ul/li/a/span[2]")
-    click_element(driver, "//*[@id='brand_search_line_0']/div[2]/ul/li/ul/li[2]/a/div")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "BRAND_input")))
-    driver.find_element_by_id("BRAND_input").send_keys(X['编号'])
-    time.sleep(5)  # 操作太快会有弹窗,一段时间后自动消失
-    driver.find_element_by_id("BRAND_input").send_keys(Keys.ENTER)
-    time.sleep(10)
-    # 提取第一页内容
-    print('3')
-    driver.save_screenshot("debug3.png")
-    results = []
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='0']/td[7]")))
-    for i in range(0, 30, 1):
-        result = driver.find_element_by_xpath("//*[@id='%d']/td[7]" % i).get_attribute("title")
-        results.append(result)
-    print(results)
-    # 判断匹配程度，并排名
-    ranks = []
-    for result in results:
-        xsd = difflib.SequenceMatcher(None, result, X['编号']).quick_ratio()
-        ranks.append(xsd)
-        print(result, xsd)
-    shot_index = ranks.index(max(ranks))
-    print(max(ranks), shot_index, type(shot_index))
-    # 设置屏幕尺寸,并截取整个屏幕
-    # driver.set_window_size(1920, 3000)
-    click_element(driver, "//*[@id='%d']/td[7]/em" % shot_index, timeout=10)
-    time.sleep(20)
-    driver.save_screenshot("%s.png" % X['编号'])
+
+    ### 获取数据
+    test_cnts = 0
+    while True:
+        print('#####进行第%d次测试#####' % test_cnts)
+        driver.get("https://www3.wipo.int/branddb/en/#")
+        ### 进行操作
+        # 点击'FILTER BY'一侧
+        print('1')
+        driver.save_screenshot("debug1.png")
+        click_element(driver, "//*[@id='source_filter']/div[1]/div/div[6]/div/a[60]/div[1]/label")
+        click_element(driver, "//*[@id='ui-id-10']/span")
+        click_element(driver, "//label[@for='ACT_check']")
+        click_element(driver, "//label[@for='PEND_check']")
+        click_element(driver, "//*[@id='status_filter']/a/span")
+        # 点击'SEARCH BY'一侧
+        print('2')
+        driver.save_screenshot("debug2.png")
+        click_element(driver, "//*[@id='brand_search_line_0']/div[2]/ul/li/a/span[2]")
+        click_element(driver, "//*[@id='brand_search_line_0']/div[2]/ul/li/ul/li[2]/a/div")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "BRAND_input")))
+        driver.find_element_by_id("BRAND_input").send_keys(X['编号'])
+        time.sleep(5)  # 操作太快会有弹窗,一段时间后自动消失
+        driver.find_element_by_id("BRAND_input").send_keys(Keys.ENTER)
+        time.sleep(10)
+        # 提取第一页内容
+        print('3')
+        driver.save_screenshot("debug3.png")
+        results = []
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='0']/td[7]")))
+        for i in range(0, 30, 1):
+            result = driver.find_element_by_xpath("//*[@id='%d']/td[7]" % i).get_attribute("title")
+            results.append(result)
+        print(results)
+        # 判断匹配程度，并排名
+        ranks = []
+        for result in results:
+            xsd = difflib.SequenceMatcher(None, result, X['编号']).quick_ratio()
+            ranks.append(xsd)
+            print(result, xsd)
+        shot_index = ranks.index(max(ranks))
+        print(max(ranks), shot_index, type(shot_index))
+        # 设置屏幕尺寸,并截取整个屏幕
+        # driver.set_window_size(1920, 3000)
+        click_element(driver, "//*[@id='%d']/td[7]/em" % shot_index, timeout=10)
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "documentContent")))
+        driver.save_screenshot("%s-%s.png" % (X['编号'], time.strftime("%Y%m%d%H%M%S", time.localtime())))
+
+        ### 进行下一次测试
+        test_cnts = test_cnts + 1
+        time.sleep(1)
+
